55464e9eb143c9fe5aea479fb7c07616f3711579
Marcia P. Moreno <mapat.soad@gmail.com>
2024-12-18 13:47:39 -0300

    Update uso-de-classes01.kt

--- uso-de-classes01.kt
+++ uso-de-classes01.kt
@@ -6,18 +6,23 @@ fun main(){
     contatos.id++
     contatos.email = "email-01@gmail.com"
     println("ID CALL: "+contatos.id+" - "+contatos.email)
+    
     contatos.id++
     contatos.email = "email-02@gmail.com"
     println("ID CALL: "+contatos.id+" - "+contatos.email)
+    
     contatos.id++
     contatos.email = "email-03@gmail.com"
     println("ID CALL: "+contatos.id+" - "+contatos.email)
+    
     contatos.id++
     contatos.email = "email-04@gmail.com"
     println("ID CALL: "+contatos.id+" - "+contatos.email)
+    
     contatos.id++
     contatos.email = "email-05@gmail.com"
     println("ID CALL: "+contatos.id+" - "+contatos.email)
+    
     contatos.id++
     contatos.email = "email-06@gmail.com"
     println("ID CALL: "+contatos.id+" - "+contatos.email)
