6ef063a3628ed1d86aefb64b8f229d9feffc41a5
Marcia P. Moreno <mapat.soad@gmail.com>
2024-12-18 12:54:09 -0300

    Create Usando-prefixos.kt

---
+++ Usando-prefixos.kt
@@ -0,0 +1,12 @@
+fun umaMensagem(mensagem: String) {
+    print(mensagem)
+}
+
+fun umaMensagemComPrefixo(mensagem: String, prefixo: String = "Info"){
+	print("$prefixo: dois hamburgueres de frango, $mensagem")
+}
+
+fun main(){
+	umaMensagem("Olá! ")
+    umaMensagemComPrefixo(prefixo = "Eu quero fazer um pedido", mensagem="Por gentileza!...")
+}
