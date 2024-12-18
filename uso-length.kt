fac7a45b84f6950b498a0efedc07416dda15d275
Marcia P. Moreno <mapat.soad@gmail.com>
2024-12-18 13:24:20 -0300

    Create uso-length.kt

---
+++ uso-length.kt
@@ -0,0 +1,13 @@
+fun descricao( umaString: String?):String{
+    if(umaString != null && umaString.length >=0 ){
+        return "O tamanho da variável é ${umaString.length}"
+    }else{
+		    return "Esta variável não recebeu qualquer valor. O tamanho é nulo"
+    }
+}
+
+fun main(){
+    println(descricao(null))
+    println(descricao("  "))
+    println(descricao("dio.me"))
+}
