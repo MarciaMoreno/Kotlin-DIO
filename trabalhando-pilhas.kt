5065515a8e05e6e146e0250af863d1f59ffc1419
Marcia P. Moreno <mapat.soad@gmail.com>
2024-12-18 15:49:03 -0300

    Create trabalhando-pilhas.kt

---
+++ trabalhando-pilhas.kt
@@ -0,0 +1,28 @@
+class MutableStack<E>(vararg items: E){
+	private val elements = items.toMutableList()
+    	fun push(element: E) = elements.add(element)
+        fun peek(): E = elements.last()
+        fun pop(): E = elements.removeAt(elements.size - 1)
+        fun isEmpty() = elements.isEmpty()
+        fun size() = elements.size
+    override fun toString() = "Lista dos Convidados para mesa: (${elements.joinToString()})"
+}
+
+fun main(){
+    val mesaGrupoA = MutableStack("Sonia","Paulo","Carlos","Antônio","Sousa")
+    println("Mesa A: "+mesaGrupoA)
+    
+    val mesaGrupoB = MutableStack("Mirian","Carlos","Priscila","Ana","Ines")
+    println("Mesa B: "+mesaGrupoB)
+    
+    val mesaGrupoC = MutableStack("Sidinei","Mauricio","Edgar","Fabricio","Amanda")
+    println("Mesa C: "+mesaGrupoC)
+    
+    // Inserindo mais convidados na mesa do grupo B
+    mesaGrupoB.push("Maria")
+    println(mesaGrupoB)
+    
+    //Removendo convidado da mesa do grupo C
+    mesaGrupoC.pop()
+    println(mesaGrupoC)
+}
