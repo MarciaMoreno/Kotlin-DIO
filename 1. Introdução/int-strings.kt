4453f8b123040512e59ce78277553df81d988470
Marcia P. Moreno <mapat.soad@gmail.com>
2024-12-21 15:47:55 -0300

    Update int-strings.kt

--- 1. Introdução/int-strings.kt
+++ 1. Introdução/int-strings.kt
@@ -3,19 +3,18 @@
  * e nomeando argumentos matematicos
  */
 
-
 fun soma( x: Int, y: Int) = x + y
 
 fun multiplica( x: Int, y:Int) = x * y
 
 fun concatena( x: String, y: String){
-	  print(x)
+    print(x)
     print(y)
 }
 
 fun main() {
-    println(soma(2,4))
-    println(multiplica(1,4))
+    println("O resultado da soma é: "+soma(2,4))
+    println("O resultado da multiplicação é: "+multiplica(1,4))
     val x = "Sara, "
     val y = "já está acordada!..."
     return concatena(x,y)
