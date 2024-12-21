23359276b8b1bec66a7e8e6fa83e70049e95ff3a
Marcia P. Moreno <mapat.soad@gmail.com>
2024-12-21 17:07:49 -0300

    Create uso-variaveis01.kt

---
+++ 1. Introdução/uso-variaveis01.kt
@@ -0,0 +1,18 @@
+/*
+ * VAR é uma variável mutável 
+ * VAL é uma variável imutável
+ */
+
+fun main() {
+
+    var a = "initial"           
+	  println(a)
+    a = "final"
+    println(a)
+    
+    val b: Int = 10
+    val c = 3
+    println(b)
+    println(c)
+    
+}
