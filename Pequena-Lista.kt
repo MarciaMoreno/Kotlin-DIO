d46072e088ef3e02ed1db4c2b92a44847ffa117e
MarciaMoreno <mapat.soad@gmail.com>
2024-12-21 07:12:59 -0300

    Atualização das subpastas

--- Pequena-Lista.kt
+++
@@ -1,6 +0,0 @@
-fun main(args: Array<String>) {
-    val bolos = listOf("Cenoura","Cream-Cheese","Chocolate")
-    for(bolo in bolos){
-		println("Hum! Isto é um bolo de $bolo. Experimente!")
-    }
-}
