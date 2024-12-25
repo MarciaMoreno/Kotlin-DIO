83356f72df075bc3541c451f09d79e8c1e7a4386
Marcia P. Moreno <mapat.soad@gmail.com>
2024-12-25 17:23:42 -0300

    Create uso-de-loops-01.kt

---
+++ 2. Controle de Fluxo/uso-de-loops-01.kt
@@ -0,0 +1,10 @@
+/*
+ * Exemplo de uso da função com loops... 
+*/
+
+fun main(){
+	  val cakes = listOf("Cenoura","Queijo-Branco","Chocolate")
+    for (cake in cakes){
+  		  println("Hummm!... Isto é um bolo de $cake.")
+    }
+}
