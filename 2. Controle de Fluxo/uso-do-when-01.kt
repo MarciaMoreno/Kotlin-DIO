344325be8922f5176f2eefc99f4e6231f9417816
Marcia P. Moreno <mapat.soad@gmail.com>
2024-12-25 17:14:50 -0300

    Update uso-do-when-01.kt

--- 2. Controle de Fluxo/uso-do-when-01.kt
+++ 2. Controle de Fluxo/uso-do-when-01.kt
@@ -7,7 +7,7 @@ class MyCake(val cobertura: String)
 
 fun cases(obj: Any){
 	when(obj){
-		10-> println("BOLO DE CENOURA COM CHOCOLATE")
+	10-> println("BOLO DE CENOURA COM CHOCOLATE")
         11-> println("2 1/2 de farinha de trigo")
         12-> println("2 1/2 de xícara de açúcar")
         13-> println("2 cenouras grande picadas")
@@ -18,7 +18,14 @@ fun cases(obj: Any){
 }
 
 fun main(){
-    cases(10);cases(11);cases(12);cases(13);cases(14);cases(15);cases(16);    
+	cases(10)
+	cases(11)
+	cases(12)
+	cases(13)
+	cases(14)
+	cases(15)
+	cases(16); 
+	
     val myclass = MyClass("Modo de Preparo: Bata todos os ingrediente no liquidificador, exceto o chocolate.")
     val myclass1 = MyClass("Bata muito bem ate ficar homogêneo e retire para uma tigela.")
     val myclass2 = MyClass("Em seguida unte uma forma dispense a masse e coloque no forno pré-aquecido a 180º.")
